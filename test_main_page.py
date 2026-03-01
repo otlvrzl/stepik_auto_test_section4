9a62589fe947423959ec46fd0b2c0571c2326f82
otlvrzl <otlvrzl@gmail.com>
2026-03-01 13:56:17 +0300

    added login page forms checks

--- test_main_page.py
+++ test_main_page.py
@@ -11,3 +11,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_page_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
+    page.should_be_login_page()
